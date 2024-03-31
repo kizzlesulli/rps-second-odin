be914aeb01f0df5ae9fa6e8e3ebe44568bd22cbb
kizzlesulli <59520465+kizzlesulli@users.noreply.github.com>
2024-03-31 16:24:54 -0600

    add function to take paler and computer selection, play single round and return results

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
 //Get random number between 0 and three for computer choice
 
-
-
 function getComputerChoice () {
 
-    let computerChoice = Math.floor(Math.random() * 3);
+    return Math.floor(Math.random() * 3);
 
-    return computerChoice;
 }
 
-getComputerChoice();
+//Check player input vs computer choice and return results
+
+function playRound (player, computer) {
+    
+    let playerNumber ;
+    let results;
+
+    // switch (playerSelection) {
+    //     case 'rock':
+    //         playerNumber = 0
+    //         break;
+    //     case 'paper':
+    //         playerNumber = 1;
+    //         break;
+    //     case 'scissors':
+    //         playerNumber = 2;
+    // }
+
+    //get results
+    if (
+        player == 'rock' && computer == 0 ||
+        player == 'paper' && computer == 1 ||
+        player == 'scissors' && computer == 2) {
+        
+        results = 'It\'s is a tie.';
+
+    } else if (
+        player == 'rock' && computer == 2 ||
+        player == 'paper' && computer == 0 ||
+        player == 'scissors' && computer == 1) {
+        
+        results = "You win.";
+    } else if (
+        player == 'rock' && computer == 1 ||
+        player == 'paper' && computer == 2 ||
+        player == 'scissors' && computer == 0){
+        
+        results = "Computer win.";
+    }
+
+    return results;
+}
+
+let playerSelection = prompt('Rock, paper or scissors?').toLocaleLowerCase();
+let computerSelection = getComputerChoice();
+let results = playRound(playerSelection, computerSelection);
+
+console.log (playerSelection, computerSelection, results);
